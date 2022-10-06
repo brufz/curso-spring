82ef3d1cb245c3494adce99ef050bd4104f6bb5b
Bruna Franchi <bfzdev@gmail.com>
2022-10-06 11:21:01 -0300

    controller produto

---
+++ src/main/java/io/github/brufz/controller/ProdutoController.java
@@ -0,0 +1,71 @@
+package io.github.brufz.controller;
+
+import io.github.brufz.model.Cliente;
+import io.github.brufz.model.Produto;
+import io.github.brufz.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @GetMapping("/{id}")
+    public Produto getProdutoById(@PathVariable Integer id) throws ResponseStatusException {
+        return produtoRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+
+    @GetMapping
+    public List<Cliente> find(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                                .matching()
+                                .withIgnoreCase()
+                                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filtro, matcher);
+
+        return produtoRepository.findAll(example);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto post(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id,
+                        @RequestBody Produto produto){
+
+        produtoRepository.findById(id).map(produtoExistente -> {
+            produto.setId(produtoExistente.getId());
+            produtoRepository.save(produto);
+            return produtoExistente;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Produto não encontrado"));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Integer id){
+        produtoRepository.findById(id)
+                .map(produto -> {
+                    produtoRepository.delete(produto);
+                    return produto;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+}
+
+
